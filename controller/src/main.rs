9d43f7a2379ebcd7f498e16a92857eae0b336244
TheZoq2 <frans.skarman@gmail.com>
2018-10-28 19:45:54 +0100

    Migrate to interrupt based interface

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -20,6 +20,7 @@ mod voltage;
 mod keypad;
 mod keymap;
 mod interface;
+mod state;
 
 use rtfm::{Threshold, app};
 
@@ -32,9 +33,13 @@ use stm32f103xx_hal::gpio::{Output, PushPull, Floating, Input, PullDown, PullUp}
 use stm32f103xx_hal::timer::{Timer};
 use stm32f103xx_hal::pwm;
 use stm32f103xx_hal::time::Hertz;
-use rt::ExceptionFrame;
-
 use stm32f103xx::{TIM2, TIM3};
+use stm32f103xx::{EXTI, NVIC};
+use rt::ExceptionFrame;
+use rtfm::Resource;
+
+use state::State;
+
 
 type Lcd = hd44780_driver::HD44780<
     // Delay
@@ -69,11 +74,26 @@ app! {
         static KEYPAD: Keypad;
         static KEY_DELAY_TIMER: Timer<TIM3>;
         static OUTPUT_SENSOR: PA8<Input<PullUp>>;
+        static STATE: State;
+        static INTERRUPT_CONTROLLER: NVIC;
+        static EXTI_CONTROLLER: EXTI;
     },
 
     idle: {
-        resources: [PWM, LCD, KEYPAD, KEY_DELAY_TIMER, OUTPUT_SENSOR]
+        resources: [KEYPAD, KEY_DELAY_TIMER, STATE, LCD, INTERRUPT_CONTROLLER]
     },
+
+    tasks: {
+        EXTI1: {
+            path: state_changed,
+            resources: [PWM, LCD, STATE]
+        },
+
+        EXTI9_5: {
+            path: output_switch_changed,
+            resources: [OUTPUT_SENSOR, INTERRUPT_CONTROLLER, STATE, EXTI_CONTROLLER]
+        }
+    }
 }
 
 
@@ -86,21 +106,25 @@ fn init(p: init::Peripherals) -> init::LateResources {
     let mut afio = p.device.AFIO.constrain(&mut rcc.apb2);
     let syst = p.core.SYST;
 
+    // Disable the JTAG hardware to free up PB3 and 4
     afio.mapr.disable_jtag();
 
+    // Timer used for preventing bouncy buttons
     let timer = Timer::tim3(p.device.TIM3, Hertz(100), clocks, &mut rcc.apb1);
 
-    let delay = stm32f103xx_hal::delay::Delay::new(syst, clocks);
-
-
+    ////////////////////////////////////////////////////////////////////////////////
+    //                              PWM
+    ////////////////////////////////////////////////////////////////////////////////
     let pwm_pin = gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl);
     let mut pwm = p.device.TIM2.pwm(pwm_pin, &mut afio.mapr, Hertz(20_000), clocks, &mut rcc.apb1);
     pwm.set_duty(0);
     pwm.enable();
 
     ////////////////////////////////////////////////////////////////////////////////
-    // LCD
+    //                              LCD
     ////////////////////////////////////////////////////////////////////////////////
+    let delay = stm32f103xx_hal::delay::Delay::new(syst, clocks);
+
     let mut lcd = hd44780_driver::HD44780::new_4bit(
             // rs
             gpioa.pa10.into_push_pull_output(&mut gpioa.crh),
@@ -122,7 +146,7 @@ fn init(p: init::Peripherals) -> init::LateResources {
     lcd.write_str("Hello, world!");
 
     ////////////////////////////////////////////////////////////////////////////////
-    // Keypad
+    //                              Keypad
     ////////////////////////////////////////////////////////////////////////////////
 
     let keypad = Keypad::new(
@@ -139,74 +163,126 @@ fn init(p: init::Peripherals) -> init::LateResources {
             ]
         );
 
+    ////////////////////////////////////////////////////////////////////////////////
+    //                          Output switch
+    ////////////////////////////////////////////////////////////////////////////////
+    let output_sensor = gpioa.pa8.into_pull_up_input(&mut gpioa.crh);
+
+    // Mask exti8
+    p.device.EXTI.imr.modify(|_r, w| w.mr8().set_bit());
+    // Trigger on both falling and rising edge
+    p.device.EXTI.rtsr.modify(|_r, w| w.tr8().set_bit());
+    p.device.EXTI.ftsr.modify(|_r, w| w.tr8().set_bit());
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //                          Other
+    ////////////////////////////////////////////////////////////////////////////////
+    let state = State::new(output_sensor.is_low());
 
     init::LateResources {
         PWM: pwm,
         LCD: lcd,
         KEYPAD: keypad,
         KEY_DELAY_TIMER: timer,
-        OUTPUT_SENSOR: gpioa.pa8.into_pull_up_input(&mut gpioa.crh),
+        OUTPUT_SENSOR: output_sensor,
+        STATE: state,
+        INTERRUPT_CONTROLLER: p.core.NVIC,
+        EXTI_CONTROLLER: p.device.EXTI,
     }
 }
 
-fn idle(_t: &mut Threshold, r: idle::Resources) -> ! {
-    let min_voltage: f32 = 1.295;
-    let max_voltage: f32 = 18.95 + min_voltage;
-    let voltage_multiplyer = 1.05;
-
+fn idle(t: &mut Threshold, mut r: idle::Resources) -> ! {
     let mut last_key = None;
 
     let mut interface_state = interface::State::Start;
 
     loop {
-        {
-            let key = r.KEYPAD.read_first_key();
+        let key = r.KEYPAD.read_first_key();
 
-            match key {
-                Some(coords) => {
-                    let key_char = keypad::translate_coordinate(coords, &keymap::KEYMAP);
+        match key {
+            Some(coords) => {
+                let key_char = keypad::translate_coordinate(coords, &keymap::KEYMAP);
 
-                    if Some(key_char) != last_key {
-                        // Process the key
-                        let (new_state, command) = interface_state.update(key_char);
-                        interface_state = new_state;
+                if Some(key_char) != last_key {
+                    // Process the key
+                    let (new_state, command) = interface_state.update(key_char);
+                    interface_state = new_state;
 
-                        if let Some(interface::Command::Voltage(val)) = command {
-                            let duty_percentage = voltage::pwm_percentage_for_voltage(
-                                val,
-                                min_voltage,
-                                max_voltage
-                            );
-
-                            let duty = ((r.PWM.get_max_duty() as f32) * duty_percentage * voltage_multiplyer);
-                            r.PWM.set_duty(duty as u16)
-                        }
-
-                        r.LCD.clear();
-                        r.LCD.write_str(&interface_state.get_display().unwrap());
-
-                        r.LCD.set_cursor_pos(40);
-                        if r.OUTPUT_SENSOR.is_high() {
-                            r.LCD.write_str("Output off");
-                        }
-                        else {
-                            r.LCD.write_str("Output on");
-                        }
-
-                        last_key = Some(key_char)
+                    if let Some(interface::Command::Voltage(val)) = command {
+                        r.STATE.claim_mut(t, |state, _t| {
+                            state.set_voltage(val);
+                        });
+                        r.INTERRUPT_CONTROLLER.claim_mut(t, |nvic, _t| {
+                            nvic.set_pending(stm32f103xx::Interrupt::EXTI1);
+                        });
                     }
 
-                    r.KEY_DELAY_TIMER.start(Hertz(100));
-                    block!(r.KEY_DELAY_TIMER.wait());
+                    let message = interface_state.get_display().unwrap();
+
+
+                    r.LCD.claim_mut(t, |lcd, _t| {
+                        write_line(0, lcd, &message);
+                    });
+
+                    last_key = Some(key_char)
                 }
-                None => {
-                    last_key = None
-                }
+
+                r.KEY_DELAY_TIMER.start(Hertz(100));
+                block!(r.KEY_DELAY_TIMER.wait());
+            }
+            None => {
+                last_key = None
             }
         }
     }
 }
 
+fn state_changed(t: &mut Threshold, mut r: EXTI1::Resources) {
+    let min_voltage: f32 = 1.295;
+    let max_voltage: f32 = 18.95 + min_voltage;
+    let voltage_multiplyer = 1.05;
+
+    let duty_percentage = voltage::pwm_percentage_for_voltage(
+        r.STATE.output_voltage(),
+        min_voltage,
+        max_voltage
+    );
+
+    // Write the current status
+    write_line(1, &mut r.LCD, &r.STATE.get_display().unwrap());
+
+    let duty = (r.PWM.get_max_duty() as f32) * duty_percentage * voltage_multiplyer;
+    r.PWM.set_duty(duty as u16);
+}
+
+
+fn output_switch_changed(t: &mut Threshold, mut r: EXTI9_5::Resources) {
+    r.STATE.set_output_switch_state(r.OUTPUT_SENSOR.is_low());
+
+    // Clear this interrupt and raise the state change interrupt
+    r.INTERRUPT_CONTROLLER.set_pending(stm32f103xx::Interrupt::EXTI1);
+    r.EXTI_CONTROLLER.pr.modify(|_, w| w.pr8().set_bit());
+}
+
+
+/**
+  Writes a single line to the LCD with padding to clear the previous content
+*/
+fn write_line(line: u8, lcd: &mut Lcd, message: &str) {
+    if line == 0 {
+        lcd.set_cursor_pos(0);
+    }
+    else {
+        lcd.set_cursor_pos(40);
+    };
+    const LINE_LENGTH: usize = 16;
+    let amount_of_padding = LINE_LENGTH - message.len();
+
+    lcd.write_str(message);
+    for _ in 0..amount_of_padding {
+        lcd.write_char(' ');
+    }
+}
 
 exception!(HardFault, hard_fault);
 
---
+++ controller/src/state.rs
@@ -0,0 +1,60 @@
+use arrayvec::{ArrayString, CapacityError};
+use itoa;
+
+pub struct State {
+    set_voltage: f32,
+    output_switch_state: bool,
+    pub output_enabled: bool
+}
+
+impl State {
+    pub fn new(output_switch_state: bool) -> Self {
+        Self {
+            set_voltage: 0.,
+            output_switch_state,
+            output_enabled: false
+        }
+    }
+
+
+    pub fn output_voltage(&self) -> f32 {
+        if self.output_enabled {
+            self.set_voltage
+        }
+        else {
+            0.
+        }
+    }
+
+    pub fn set_output_switch_state(&mut self, new: bool) {
+        self.output_switch_state = new;
+        if new == false {
+            self.output_enabled = true;
+        }
+    }
+
+    pub fn set_voltage(&mut self, voltage: f32) {
+        self.set_voltage = voltage;
+    }
+
+    pub fn get_display(&self) -> Result<ArrayString<[u8; 32]>, CapacityError<&str>> {
+        let mut result = ArrayString::new();
+        let mut buffer = itoa::Buffer::new();
+        result.push_str(buffer.format((self.set_voltage * 1000.) as u16 ));
+        result.push_str(" mV ");
+
+        if self.output_enabled {
+            if self.output_switch_state {
+                result.push_str("On");
+            }
+            else {
+                result.push_str("Off");
+            }
+        }
+        else {
+            result.push_str("Disabled");
+        }
+
+        Ok(result)
+    }
+}
